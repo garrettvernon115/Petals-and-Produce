de46ad963f3babf06c6032b35b96b123c8c46b79
Kat Falcone <falcon15@email.franklin.edu>
2025-06-22 21:22:26 -0400

    finish auth registration and login implementation

---
+++ backend/src/main/java/com/petalsandproduce/backend/repository/UserRepository.java
@@ -0,0 +1,8 @@
+package com.petalsandproduce.backend.repository;
+
+import com.petalsandproduce.backend.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+    User findByUsername(String username);
+}
---
+++ backend/src/main/java/com/petalsandproduce/backend/util/JwtUtil.java
@@ -0,0 +1,38 @@
+package com.petalsandproduce.backend.util;
+
+import io.jsonwebtoken.*;
+import org.springframework.stereotype.Component;
+
+import java.util.Date;
+
+@Component
+public class JwtUtil {
+    private final String SECRET_KEY = "super-secret-key"; // To replace with env var in production
+    private final long EXPIRATION_MS = 3600000; //set to 1 hr
+
+    public String generateToken(String username) {
+        return Jwts.builder()
+                .setSubject(username)
+                .setIssuedAt(new Date())
+                .setExpiration(new Date(System.currentTimeMillis() + EXPIRATION_MS))
+                .signWith(SignatureAlgorithm.HS512, SECRET_KEY)
+                .compact();
+    }
+
+    public String extractUsername(String token) {
+        return Jwts.parser()
+                .setSigningKey(SECRET_KEY)
+                .parseClaimsJws(token)
+                .getBody()
+                .getSubject();
+    }
+
+    public boolean validateToken(String token) {
+        try {
+            Jwts.parser().setSigningKey(SECRET_KEY).parseClaimsJws(token);
+            return true;
+        } catch (JwtException e) {
+            return false;
+        }
+    }
+}
