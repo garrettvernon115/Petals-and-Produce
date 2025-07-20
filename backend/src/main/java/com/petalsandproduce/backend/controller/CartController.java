4340b07b89f040caaef0220eb1271e943cec820b
dariendalton17 <dariendalton17@gmail.com>
2025-07-19 23:39:23 -0400

    small changes

--- backend/src/main/java/com/petalsandproduce/backend/controller/CartController.java
+++ backend/src/main/java/com/petalsandproduce/backend/controller/CartController.java
@@ -1,9 +1,7 @@
 package com.petalsandproduce.backend.controller;
-
-
+ 
 import java.util.ArrayList;
 import java.util.List;
-
 import java.util.Optional;
  
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,10 +10,8 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
-
-import org.springframework.web.bind.annotation.GetMapping;
-
 import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -23,7 +19,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.petalsandproduce.backend.DTO.CartItemResponse;
-
 import com.petalsandproduce.backend.exception.ProductNotFoundException;
 import com.petalsandproduce.backend.model.Cart;
 import com.petalsandproduce.backend.model.CartItem;
@@ -85,7 +80,6 @@ public class CartController {
         return ResponseEntity.ok("Item added to cart successfully.");
     }
 
-
     @GetMapping("/cart")
     public ResponseEntity<?> getCart(HttpSession session) {
         Authentication auth = SecurityContextHolder.getContext().getAuthentication();
@@ -115,7 +109,6 @@ public class CartController {
 
         return ResponseEntity.ok(response);
         }
-    }
  
     @PostMapping("/cart/update")
     public ResponseEntity<?> updateCartItem(@RequestBody UpdateCartRequest request, HttpSession session) {
