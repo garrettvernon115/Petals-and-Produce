df3aba1f2ec09827e81551e514749932a6b1f5b7
garrettvernon115 <gvernon115@gmail.com>
2025-07-25 19:14:13 -0400

    Create an Admin Endpoint to Update Order Status - Backend

---
+++ backend/src/main/java/com/petalsandproduce/backend/DTO/UpdateOrderStatusRequest.java
@@ -0,0 +1,13 @@
+package com.petalsandproduce.backend.DTO;
+
+public class UpdateOrderStatusRequest {
+    private String status;
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+}
--- backend/src/main/java/com/petalsandproduce/backend/model/OrderStatus.java
+++ backend/src/main/java/com/petalsandproduce/backend/model/OrderStatus.java
@@ -2,9 +2,13 @@ package com.petalsandproduce.backend.model;
 
 
 public enum OrderStatus {
-    PENDING,        
-    PAID,           
-    SHIPPED,        
-    DELIVERED,      
-    CANCELLED       
+    PENDING,
+    PAID,
+    SHIPPED,
+    DELIVERED,
+    CANCELLED,
+
+    PLACED,
+    READY_FOR_PICKUP,
+    PICKED_UP
 }
